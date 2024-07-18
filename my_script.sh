2b24ae5ed8c1d2be210935dc4dd9c07fab88749e
juanBailon <66891215+juanbailon@users.noreply.github.com>
2024-07-17 19:56:35 -0500

    Update my_script.sh

--- my_script.sh
+++ my_script.sh
@@ -20,7 +20,5 @@ wget https://github.com/juanbailon/test-bin-download/raw/main/test_sel_bin
 chmod +x test_sel_bin
 ./test_sel_bin > sel_bin.logs
 
-wget https://github.com/juanbailon/test-bin-download/raw/main/main
-chmod +x main
 
 timedatectl set-timezone America/Bogota
